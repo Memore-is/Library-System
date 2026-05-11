45415f5942d99682171e509b89f8513a9c0b34ad
Zarin <226439417+Memore-is@users.noreply.github.com>
2026-05-11 15:45:03 -0400

    fix main method

--- Library System/src/libraryproject/Main.java
+++ Library System/src/libraryproject/Main.java
@@ -2,6 +2,6 @@ package libraryproject;
 
 public class Main {
 public static void main(String[] args) {
-      javax.swing.SwingUtilities.invokeLater(() -> new LibraryGUI());
+      new LibraryGUI();
     }
 }
